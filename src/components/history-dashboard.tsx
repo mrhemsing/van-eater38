d21d1b4af8264485314f3d49cb65e22f9f89daae
Matt Hemsing <46225546+mrhemsing@users.noreply.github.com>
2026-02-19 10:57:52 -0800

    Reduce desktop description text size by 1px

--- src/components/history-dashboard.tsx
+++ src/components/history-dashboard.tsx
@@ -167,11 +167,11 @@ export function HistoryDashboard({ versions }: { versions: Version[] }) {
                             dense
                           />
                           {restaurant.descriptionText ? (
-                            <div className="border-b border-neutral-800/80 py-2 text-neutral-300 lg:pr-20 lg:text-base">
+                            <div className="border-b border-neutral-800/80 py-2 text-neutral-300 lg:pr-20 lg:text-[15px]">
                               {restaurant.descriptionText}
                             </div>
                           ) : (
-                            <div className="border-b border-neutral-800/80 py-2 text-neutral-500 lg:pr-20 lg:text-base">
+                            <div className="border-b border-neutral-800/80 py-2 text-neutral-500 lg:pr-20 lg:text-[15px]">
                               Description: Not available
                             </div>
                           )}
